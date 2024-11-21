4e741de0fd75d85cc0c3cc64915163f61f7f4737
llookpear <127084768+llookpear@users.noreply.github.com>
2024-11-21 17:09:33 +0700

    1.2.12

--- lib/utils/api_constant/api_constant.dart
+++ lib/utils/api_constant/api_constant.dart
@@ -1,41 +1,30 @@
 class APIConstance {
-
   String pathMapping(String api) {
     String? url;
-
     return url ?? _apiBaseUrlToken;
   }
-
   // static String baseURL = "http://172.10.1.180:8080";
   static String baseURL = "https://hrm.up.railway.app";
   final String _apiBaseUrlToken = "$baseURL/";
-
   final String apiLogin = "login";
   final String apiUserDetail = "user-detail";
   final String apiUpdateUserDetail = "update-user";
   final String apiUpdatePassword =   "update-password";
-
   final String apiLeaveTotal = "leave-total";
   final String apiLeaveQuota = "leave-quota";
-
   final String apiOvertime= "ot-total";
   final String apiOvertimeRequest= "add-overtime";
-
   final String apiCheckInOutWorkday= "check-in-out-workday";
-
   final String apiLeaveRequest = "add-leave-request";
   final String apiLeaveType = "leave-type";
-
   final String apiOTCalendar= "ot-calendar";
   final String apiLeaveCalendar= "leave-calendar";
   //----------------------------------------------------------------------------//
   final String apiSalaryCertificateRequest= "add-salary-certificate-request";
   final String apiPaySlip= "select-payslip";
   final String apiPayslipDetail= "payslip-detail";
-
   /// ResponseType.bytes รับด้วย Uint8List
   final String apiRequestPayslip= "requestPayslip";
-
   final String apiLeaveTakenAndQuota= "leaveTaken-quota";
   final String apiLeaveStatus= "leave-status";
   final String apiLeaveHistory= "leave-history";
@@ -48,14 +37,10 @@ class APIConstance {
   final String apiBranch = "select-branch";
   final String apiCheckInOut = "add-check-in-out";
   final String apiCheckInOutTime = "check-in-out-time";
-
-  //--------------------------------------------------------------------------//
-  /// ResponseType.bytes รับด้วย Uint8List
   final String apiGetPDF = "getPDF";
   static  String getImage(String userImage) {
       return "$baseURL/getImage/$userImage";
   }
-
   //-----------------------------------Addmin-----------------------------//
   final String apiAllLeave= "select-all-leave";
   final String apiAllOvertime= "select-all-overtime";
@@ -70,3 +55,6 @@ class APIConstance {
     return list.where((element) => element == url).isNotEmpty;
   }
 }
+
+
+
